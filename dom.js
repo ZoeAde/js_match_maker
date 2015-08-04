d6401c527a23c8e44331b58725d5753c9e9df93a
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-04 14:24:13 -0600

    adding key value pair to dom

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
 function test(array) {
   for (var i = 0; i < array.length; i++) {
-    var h3 = document.createElement('h3');
-    h3.innerHTML = arrayValue + [i+1];
-    document.body.appendChild(h3);
+    // var h3 = document.createElement('h3');
+    // h3.innerHTML = array + [i+1];
+    // document.body.appendChild(h3);
     for (key in array[i]) {
       var p = document.createElement('p');
-      p.innerHTML = array[i][key];
+      p.innerHTML = key + ": " + array[i][key];
       document.body.appendChild(p);
     }
   }
--- main.js
+++ main.js
@@ -10,13 +10,9 @@ var addPeople = function() {
 
     for (var j = 0; j < totalPeople; j++) {
       var personName = prompt("What is the name of " + peopleTypes + " " + [j+1] + "?");
-        // if (studentName !== 'string') {
-        //   prompt("Please enter a valid name.");
-        // }
+
       var personPhone = prompt("What is the phone number of " + peopleTypes + " " + [j+1] + "?(ex. XXX-XXX-XXXX)");
-        // if (studentPhone.length !== 12) {
-        //   prompt("Please enter a valid phone number.");
-        // }
+
       var personCity = prompt("What city is "+ peopleTypes + " " + [j+1]);
       peopleArray.push({name: personName, phone: personPhone, city: personCity});
       }
