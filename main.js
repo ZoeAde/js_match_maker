df349f0559ec8736a4b25fcf41148330f89ec415
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-15 14:23:32 -0600

    refactoring as table

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-var peopleType = ['Student', 'Mentor'];
-var peopleArray = [];
-var totalPeople;
+$(document).on('ready', function() {
+  $('#studentSection').hide();
+  $('#mentorSection').hide();
 
-var addPeople = function() {
 
-  for (var i = 0; i < peopleType.length; i++) {
-    var peopleTypes = peopleType[i];
-    totalPeople = prompt("How many " + peopleTypes +   "s are there?");
-
-    for (var j = 0; j < totalPeople; j++) {
-      var personName = prompt("What is the name of " + peopleTypes + " " + [j+1] + "?");
-
-      var personPhone = prompt("What is the phone number of " + peopleTypes + " " + [j+1] + "?(ex. XXX-XXX-XXXX)");
-
-      var personCity = prompt("What city is "+ peopleTypes + " " + [j+1]);
-      peopleArray.push({name: personName, phone: personPhone, city: personCity});
-      }
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    if ($('#category').val() === "student") {
+      $('#studentSection').show();
+      $('#studentTableBody').append('<tr><td>' + $('#name').val() + '</td><td>' + $('#number').val() + '</td></tr>');
+    } else {
+      $('#mentorSection').show();
+      $('#mentorTableBody').append('<tr><td>' + $('#name').val() + '</td><td>' + $('#number').val() + '</td></tr>');
     }
-    console.log(peopleArray);
-   return peopleArray;
-};
+  });
+});
 
-addPeople();
